1569ac522c491438f5dd3c28669e5b5423a4f2ef
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-06 19:40:16 -0300

    CRUD no backend

--- services/src/controllers/ApiController.js
+++ services/src/controllers/ApiController.js
@@ -1,24 +1,61 @@
 import ModelUser from '../model/User.js'
 
 function index(req, res){
-    ModelUser.find().sort({createAt: 'desc'}).then(users => {
-        res.status(200).json({users})
-    })
+    try {
+        ModelUser.find().sort({createAt: 'desc'}).then(users => {
+            res.status(200).json({users})
+        })
+    } catch (error) {
+        res.status(404).json(error)
+    }
 }
 
 function login(req, res){
     const {name, email, password} = req.body
 
-    new ModelUser({
-        name,
-        email,
-        password
-    }).save()
-        .then(() => res.send('Pronto'))
-        .catch(erro => res.send(erro))
+    if (!name ||!email ||!password || email.trim()) {
+        res.status(400).json({error: 'Informações inválidas'})
+        return
+    }
+
+    try {
+        new ModelUser({
+            name,
+            email,
+            password
+        }).save()
+        .then(() => res.status(200).res.json({message: 'Usuario salvo com sucesso!'}))
+        
+    } catch (error) {
+        res.status(404).json(error)
+    }
+}
+
+function deleteUser(req, res){
+
+    const id = req.params.id
+
+    try {
+        ModelUser.findByIdAndDelete(id).then(() => res.status(200).json({message: 'Usuario excluído com sucesso!'}))
+    } catch (error) {
+        res.status(404).json(error)
+    }
+}
+
+function editUser(req, res){
+    const id = req.params.id
+    const {email, password} = req.body
+    
+    try {
+        ModelUser.findByIdAndUpdate(id, {email, password}).then(() => res.status(200).json({message: 'Usuario editado com sucesso!'}))
+    } catch (error) {
+        res.status(404).json(error)
+    }
 }
 
 export  {
     index,
-    login
+    login,
+    deleteUser,
+    editUser
 }
--- services/src/routes/api.js
+++ services/src/routes/api.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {index, login} from '../controllers/ApiController.js'
+import {index, login, deleteUser, editUser} from '../controllers/ApiController.js'
 
 const router = express.Router()
 
 router.get('/users', index)
 router.post('/login', login)
+router.delete('/users/:id', deleteUser)
+router.put('/users/:id', editUser)
 
 export default router
