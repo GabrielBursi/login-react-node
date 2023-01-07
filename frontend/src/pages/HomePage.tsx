f048fb8113368c2103cf821662e51dad90854ce2
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-06 21:35:59 -0300

    começando a home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import { ApiError } from "../api/ApiError";
 import { getAll } from "../api/ApiServices";
 import ApiType from "../types/ApiType";
 
+import './styles/home.css'
+
 function HomePage() {
 
     const [users, setUsers] = useState<ApiType[]>([]);
@@ -18,11 +20,22 @@ function HomePage() {
     }, []);
 
     return (
-        <div className="container">
-            <h1>uri da API: http://localhost:3333/users</h1>
-            <span>Apenas usuarios cadastrados conseguem acessar essa página</span>
-            <div className="user-data">
-
+        <div className="container home">
+            <div className="api-text">
+                <h1>uri da API: <a href="http://localhost:3333/users" target="_blank" rel="noreferrer">http://localhost:3333/users</a></h1>
+                <span>Apenas usuarios cadastrados conseguem acessar essa página.</span>
+            </div>
+            <div className="user-container">
+                <span>Todos usuarios cadastrados no banco de dados MongoDB:</span>
+                <code className="user-data">
+                    {users.map((user) => {
+                        return (
+                            <span key={user._id}>
+                                Nome: {user.name} Email: {user.email} Senha: {user.password}
+                            </span>
+                        )
+                    })}
+                </code>
             </div>
         </div>
     );
