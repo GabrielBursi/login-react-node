51ec863c239fe34eb37a3a2c27a78cfadd10ab28
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-26 10:41:49 -0300

    criando container

---
+++ frontend/src/components/Container/Container.tsx
@@ -0,0 +1,33 @@
+import { Box, Paper } from "@mui/material";
+import { Children } from "../../types";
+
+function Container({children}: Children) {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "90vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            paddingTop={2}
+        >
+            <Box
+                component={Paper}
+                sx={{
+                    width: "40%",
+                    height: "70%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                padding={2}
+            >
+                {children}
+            </Box>
+        </Box>
+    );
+}
+
+export default Container;
