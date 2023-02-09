adfcf4d3602b3f72f5ec7df8c6249aded3ddde14
Gabriel Bursi <gabriellbursii@gmail.com>
2023-02-09 08:57:02 -0300

    teste cyclic

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
-import morgan from 'morgan'
 
 import connectDB from './database/db.js'
 connectDB()
@@ -13,7 +12,6 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(morgan('tiny'))
 
 app.use('/', router)
 
